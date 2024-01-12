5d7ef72bd4232db15652666a7ff2d4f6abf31d95
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2024-01-12 16:43:54 +0000

    Update

--- Tree.java
+++ Tree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Tree {
     static class Node {
         int data;
@@ -28,18 +29,57 @@ public class Tree {
 
     public static void preorder(Node root) {
         if (root == null) {
-            System.out.print("-1" + " ");
+            //System.out.print("-1" + " ");
             return;
         }
         System.out.print(root.data + " ");
         preorder(root.left);  // Corrected method calls
         preorder(root.right);
+        System.out.println();
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data);
+        inorder(root.right);
+    }
+    public static void layerorder(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+        }
     }
 
+
     public static void main(String args[]) {
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = {1,2,3,4,5,6};
         BTree tree = new BTree();
         Node root = tree.buildTree(nodes);
         preorder(root);
+        inorder(root);
+        layerorder(root);
     }
 }
