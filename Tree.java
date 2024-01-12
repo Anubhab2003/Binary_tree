3f1b2e044c138decbf29f2ac7c2e81c62527997b
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2024-01-12 11:53:38 +0000

    Update

--- Tree.java
+++ Tree.java
@@ -1,35 +1,45 @@
-public class Tree{
-    static class Node{
+public class Tree {
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
-
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-    static class BTree{
-        static int idx=-1;
-        public static Node buildTree(int nodes[]){
+
+    static class BTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx]==-1){
+            if (nodes[idx] == -1) {
                 return null;
-
             }
-            Node newNode=new Node(nodes[idx]);
-            newNode.left=buildTree(nodes);
-            newNode.right=buildTree(nodes);
-
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
             return newNode;
         }
     }
-    public static void main(String args[]){
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BTree tree=new BTree();
-        Node root=tree.buildTree(nodes);
-        System.out.println(root.data);
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            System.out.print("-1" + " ");
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);  // Corrected method calls
+        preorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BTree tree = new BTree();
+        Node root = tree.buildTree(nodes);
+        preorder(root);
     }
 }
