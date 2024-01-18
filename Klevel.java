1d7ad9042cedbca5ffe49b7c57b02a37b58cbb27
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2024-01-18 14:24:00 +0000

    K lkevel

--- Klevel.java
+++ Klevel.java
@@ -1,35 +1,61 @@
-public class Klevel{
+import java.util.LinkedList;
+import java.util.Queue;
 
-	static class Node{
-		int data;
-		Node left,right;
-		public Node(int data){
-			this.data=data;
-			this.left=null;
-			this.right=null;
-		}
-	}
-	public static void main(String args[]){
-		Node root=new Node(1);
-		root.left=new Node(2);
-		root.right=new Node(3);
-		root.left.left=new Node(4);
-		root.left.right=new Node(5);
-		root.right.left=new Node(6);
-		root.right.right=new Node(7);
-		int k=3;
-		klevel(root,1,k);
-	}
-	public static void klevel(Node root,int level,int k){
-		if(root==null){
-			return;
-		}
-		if(level==k){
-			System.out.print(root.data+" ");
-			return;
-		}
-		klevel(root.left,level+1,k);
-		klevel(root.right,level+1,k);
-	}
+public class Klevel {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void klevel2(Node root, int k) {
+        int l = 0;
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                l++;
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (l == k) {
+                    System.out.print(currNode.data + " ");
+                }
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        int k = 3;
+
+        // Call the method to print nodes at level k
+        klevel2(root, k);
+    }
 }
-			
