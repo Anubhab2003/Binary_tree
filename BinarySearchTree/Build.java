59924e59a5f59da844a5d59b29987127d51eee15
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2024-02-10 09:43:33 +0000

    Features

--- BinarySearchTree/Build.java
+++ BinarySearchTree/Build.java
@@ -70,6 +70,22 @@ public class Build{
         }
         return root;
     }
+    public static void printInRange(Node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
+        if(root.data>=k1&&root.data<=k2){
+            printInRange(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right,k1,k2);
+        }
+        else if(root.data<k1){
+            printInRange(root.left,k1,k2);
+        }
+        else{
+            printInRange(root.right,k1,k2);
+        }
+    }
     public static void main(String args[]){
         int values[]={8,5,3,1,4,6,10,11,14};
         Node root=null;
@@ -78,9 +94,11 @@ public class Build{
         }
         inorder(root);
         System.out.println();
-        root=delete(root,5);
-        System.out.println();
-        inorder(root);
+        // root=delete(root,5);
+        // System.out.println();
+        // inorder(root);
+        printInRange(root,5,12);
+        //inorder(root);
     }
 
     public static void inorder(Node root){
